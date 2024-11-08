2b5510079a676c8aac33fd54f751411f90f9179d
Victoria Hektor <94381284+victoria-hektor@users.noreply.github.com>
2024-11-08 14:46:52 +0000

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,16 @@
+# Variables
+age = 25 # Clear and simple
+user_name = "Victoria" # Underscores make variable names easy to read
+
+a = 25 # harder to understand
+x = "Victoria" # What even is x??
+
+a = 1 # single letter variables can be good for calculations
+b = 3
+
+# Functions
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Call your function
+greet("Victoria")
---
+++ simple-calculations.py
@@ -0,0 +1,18 @@
+# Simple Calculations
+a = 5
+b = 2
+
+# Calculations
+addition = a + b 
+subtraction = a - b
+multiplication = a * b
+division = a / b
+percentage_of_a = (a/b) * 100 # percentage of a relative to b
+
+# print the results
+print("--- calculations ---")
+print(f"Addition (a+b): {addition}")
+print(f"subtraction (a-b): {subtraction}")
+print(f"Multiplication (a*b): {multiplication}")
+print(f"Division (a/b): {division:.2f}") # limit to 2 decimal places
+print(f"Percentage of a relative to b: {percentage_of_a:.2f}") # limit to 2 decimal places
